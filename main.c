3f7185f6ccd6b731b98074c523c6843636181403
James <james.oreilly@mnsu.edu>
2022-09-13 01:19:44 -0500

    added screenshots of functioning code and changed c printing method

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 // Created by James O'Reilly on 9/13/22.
 //
 #include <stdio.h>
-#include <stdlib.h>
+
 ///recursive gcd using euclid algo
 int gcd(int p, int q){
 	if(q == 0)
@@ -12,6 +12,8 @@ int gcd(int p, int q){
 }
 ///did not code a console reader for c function
 int main() {
-	return gcd(1845, 102465);
+	printf("running GCD(1845,102465)");
+	int a = gcd(1845, 102465);
+	printf("\nResult: %d",a);
 }
 
